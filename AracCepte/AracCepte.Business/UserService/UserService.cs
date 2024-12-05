039fe948b0b3983ddeacd50d547f33dc28a242a5
omerdemirci17 <omerdemirci2004@gmail.com>
2024-12-05 21:14:24 +0300

    Update UserService.cs

--- AracCepte/AracCepte.Business/UserService/UserService.cs
+++ AracCepte/AracCepte.Business/UserService/UserService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BCrypt.Net;
 
 namespace AracCepte.Business.UserService
 {
-    internal class UserService
+    public class UserService
     {
+        public string HashPassword(string plainPassword)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(plainPassword, workFactor: 10);
+        }
+
+        public bool VerfyPassword(string plainPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainPassword, hashedPassword);
+        }
     }
 }
