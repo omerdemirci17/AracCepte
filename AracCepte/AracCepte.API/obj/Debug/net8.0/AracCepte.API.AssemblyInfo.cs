892a0fe4c8e46778eacff103e46255f0b407ffe9
omerdemirci17 <omerdemirci2004@gmail.com>
2024-12-07 16:28:14 +0300

    07.12.24 16.28

--- AracCepte/AracCepte.API/obj/Debug/net8.0/AracCepte.API.AssemblyInfo.cs
+++ AracCepte/AracCepte.API/obj/Debug/net8.0/AracCepte.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AracCepte.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f33e56441810d5f6822a0961099f67ab402d3b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcd8242bc120c828ee33ef983c58e980704f55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AracCepte.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AracCepte.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
